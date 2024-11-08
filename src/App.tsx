d82b204b6fdb397ed9202cf61cb13c141c635d4e
agungbharata <afyastudio@gmail.com>
2024-11-08 09:23:55 +0700

    hide testimoni

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
       <Hero />
       <Problems />
       <Features />
-      <Testimonials />
+      {/* <Testimonials /> */}
       <Bonuses />
       <Pricing />
       <FAQ />
